0dedcb3e6787340675d126f3a408faa4b9b2e6d6
worthyOfThrone <arshiya.ans777@gmail.com>
2022-07-24 00:02:53 +0530

    setting up inbuilt nestjs logger, and updating the prettierrc for formatting documents locally

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    /**
+     * for development/debugging purpose until all necessary services/controllers are created,
+     * setting log level to log
+     */
+    logger: ['log', 'error', 'warn'],
+  });
   await app.listen(3000);
 }
 bootstrap();
