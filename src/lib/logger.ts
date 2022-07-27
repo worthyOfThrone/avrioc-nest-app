ddd3e55dadd452f728ebc80a2f447cde14dd9628
worthyOfThrone <arshiya.ans777@gmail.com>
2022-07-27 18:02:42 +0530

    setting commong logger messages

---
+++ src/lib/logger.ts
@@ -0,0 +1,15 @@
+export enum loggerMessages {
+    GENERIC= 'the requested resource is ',
+    GENERIC_PLURAL= 'the requested resources are ',
+    FOUND= 'the resource is found',
+    NOT_FOUND= 'the resource does not exist',
+    FORBIDDON= 'the access to particular resource is forbiddon',
+    UN_AUTHORIZE= 'the user is unauthorized',
+    AUTHORIZE= 'the user is authorized',
+    CREATED= 'the resource is created',
+    UPDATED= 'the resource is updated',
+    REGISTERED= 'the user is registered',
+    ALREADY_EXISTS= 'the resource already exists',
+    DELETED= 'the resource(s) are deleted',
+    INSERTED= 'the resource(s) are inserted'
+}
